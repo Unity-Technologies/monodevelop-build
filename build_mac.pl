4b4af86c94a65cc835253132968762ed01d6b29d
Lukasz <lukaszunity@users.noreply.github.com>
2015-07-07 09:39:26 +0200

    Used remove_unwanted_addins.pl in OS X build script.

--- build_mac.pl
+++ build_mac.pl
@@ -7,6 +7,8 @@ use File::Copy;
 use File::Path;
 use File::Find qw( find );
 
+require "remove_unwanted_addins.pl";
+
 my $buildRepoRoot = File::Spec->rel2abs( dirname($0) );
 my $root = File::Spec->rel2abs( File::Spec->updir() );
 my $mdSource = "$root/monodevelop/main/build";
@@ -17,6 +19,8 @@ main();
 sub main {
 	prepare_sources();
 	setup_nant();
+
+	# Build MonoDevelop
 	build_monodevelop();
 	remove_unwanted_addins();
 
@@ -48,66 +52,6 @@ sub setup_nant
 	$ENV{PKG_CONFIG_PATH}="/Library/Frameworks/Mono.framework/Versions/Current/lib/pkgconfig";
 	$nant = "mono --runtime=v4.0.30319 $buildRepoRoot/dependencies/nant-0.93-nightly-2015-02-12/bin/NAnt.exe";
 }
-
-sub IsWhiteListed {
-	my ($path) = @_;
-	print "IsWhite: $path\n";
-	return 1 if $path =~ /main\/build\/Addins$/;
-	return 1 if $path =~ /main\/build\/Addins\/MacPlatform.xml/;
-	return 1 if $path =~ /main\/build\/Addins\/BackendBindings$/;
-
-	return 1 if $path =~ /ICSharpCode.NRefactory/;
-	return 1 if $path =~ /ILAsmBinding/;
-	return 1 if $path =~ /MonoDevelop.CSharpBinding./;
-		
-	return 1 if $path =~ /DisplayBindings$/;
-	return 1 if $path =~ /DisplayBindings\/AssemblyBrowser/;
-	return 1 if $path =~ /DisplayBindings\/HexEditor/;
-	return 1 if $path =~ /DisplayBindings\/SourceEditor/;
-	return 1 if $path =~ /\/MonoDevelop.Debugger$/;
-
-	return 1 if $path =~ /MonoDevelop.Debugger\/MonoDevelop.Debugger./;
-	return 1 if $path =~ /MonoDevelop.Debugger.Soft/;
-	
-	return 1 if $path =~ /MonoDevelop.DesignerSupport/;
-	return 1 if $path =~ /MonoDevelop.Refactoring/;
-	return 1 if $path =~ /MonoDevelop.RegexToolkit/;
-	return 1 if $path =~ /WindowsPlatform/;
-	return 1 if $path =~ /NUnit/;
-	return 1 if $path =~ /\/Xml/;
-	
-	return 0;
-}
-
-sub IsBlackListed {
-	my ($path) = @_;
-	return 1 if $path =~ /.DS_Store/;
-	return 1 if $path =~ /MonoDevelop.CBinding/;
-	return 1 if $path =~ /Addins\/AspNet/;
-	return 1 if $path =~ /MonoDevelop.DocFood/;
-	return 1 if $path =~ /MonoDevelop.VBNetBinding/;
-	return 1 if $path =~ /ChangeLogAddIn/;
-	return 1 if $path =~ /DisplayBindings\/Gettext/;
-	return 1 if $path =~ /MonoDevelop.Deployment/;	
-	return 1 if $path =~ /MonoDevelop.GtkCore/;
-	return 1 if $path =~ /MonoDevelop.PackageManagement/;
-	return 1 if $path =~ /MonoDevelop.TextTemplating/;
-	return 1 if $path =~ /MonoDevelop.WebReferences/;
-	return 1 if $path =~ /MonoDeveloperExtensions/;
-	return 1 if $path =~ /VersionControl/;
-
-	return 1 if $path =~ /Addins\/MonoDevelop.Autotools/;
-
-	return 1 if $path =~ /MonoDevelop.Debugger.Soft.AspNet/;
-	return 1 if $path =~ /MonoDevelop.Debugger.Gdb/;
-	return 1 if $path =~ /MonoDevelop.Debugger.Win32/;
-
-	# Blacklist local build of the add-ins.
-	return 1 if $path =~ /MonoDevelop.Boo.UnityScript.Addins/;
-
-	return 0;
-}
-
 
 sub build_monodevelop {
 	chdir "$root/monodevelop";
@@ -165,32 +109,6 @@ sub build_boo_unity_addins {
 	chdir "$root/MonoDevelop.Boo.UnityScript.Addins";
 	mkpath "$addinsdir/MonoDevelop.Boo.UnityScript.Addins";
 	system("xbuild /property:Configuration=Release /t:Rebuild /p:OutputPath=\"$addinsdir/MonoDevelop.Boo.UnityScript.Addins\"") && die("Failed building Unity debugger addin");
-}
-
-sub process_addin_path {
-   if (IsBlackListed($File::Find::name))
-	{
-		print "Killing $File::Find::name\n";	
-		my $fullpath = $File::Find::name;
-		print "Killing $fullpath\n";	
-		rmtree($fullpath);
-		$File::Find::prune = 1;
-		return;
-	} 
-   if (IsWhiteListed($File::Find::name))
-   {
-   		return;
-   }
-   die ("Found path in Addins folder that is not in whitelist or blacklist: $File::Find::name\n");
-}
-
-sub remove_unwanted_addins()
-{
-	chdir("$root/monodevelop");
-	find({
-	   wanted   => \&process_addin_path,
-	   no_chdir => 1,
-	}, "main/build/Addins");
 }
 
 sub package_monodevelop {
