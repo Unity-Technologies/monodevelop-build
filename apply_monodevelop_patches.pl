bb1b6e7d5f035650b3b5828b62f0f63415a41f9d
Lukasz <lukaszunity@users.noreply.github.com>
2015-07-31 09:55:53 +0200

    Added reverse_mono_develop_patches sub routine to apply_monodevelop_patches.pl

--- apply_monodevelop_patches.pl
+++ apply_monodevelop_patches.pl
@@ -11,7 +11,6 @@ sub apply_mono_develop_patches()
 	my $root = $_[0];
 	my $buildRepoRoot = $_[1];
 
-	# MonoDevelop patches
 	print "Applying monodevelop.patch\n";
 	chdir "$root/monodevelop";
 	system("git apply --ignore-space-change --ignore-whitespace $buildRepoRoot/patches/monodevelop.patch") && die("Failed to apply monodevelop.patch");
@@ -19,6 +18,21 @@ sub apply_mono_develop_patches()
 	print "Applying debugger-libs.patch\n";
 	chdir "main/external/debugger-libs";
 	system("git apply --ignore-space-change --ignore-whitespace $buildRepoRoot/patches/debugger-libs.patch") && die("Failed to apply debugger-libs.patch");
+}
+
+sub reverse_mono_develop_patches()
+{
+	my $root = $_[0];
+	my $buildRepoRoot = $_[1];
+
+	print "Reversing monodevelop.patch\n";
+	chdir "$root/monodevelop";
+	system("git apply --reverse --ignore-space-change --ignore-whitespace $buildRepoRoot/patches/monodevelop.patch") && die("Failed to reverse monodevelop.patch");
+
+	print "Reversing debugger-libs.patch\n";
+	chdir "main/external/debugger-libs";
+	system("git apply --reverse --ignore-space-change --ignore-whitespace $buildRepoRoot/patches/debugger-libs.patch") && die("Failed to reverse debugger-libs.patch");
+
 }
 
 1;
