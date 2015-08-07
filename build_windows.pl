db66b2ae34af69e47cbec5510613fe6ba4cd4069
Lukasz Paczkowski <lukasz@unity3d.com>
2015-08-07 08:48:23 +0200

    build_windows.pl: Use different 7z path on build machines.

--- build_windows.pl
+++ build_windows.pl
@@ -26,16 +26,17 @@ my $root = File::Spec->rel2abs( File::Spec->updir() );
 
 my $mdSource = "$root/monodevelop/main/build";
 
-my $nant = "";
+my $nant = "\"$buildRepoRoot/dependencies/nant-0.93-nightly-2015-02-12/bin/NAnt.exe\"";
 my $incremental = "/t:Rebuild";
 
 main();
 
 sub main {
 	prepare_sources();
+	setup_env();
+
 	install_gkt_sharp();
 	install_mono_libraries();
-	setup_nant();
 
 	# Build MonoDevelop
 	apply_mono_develop_patches($root, $buildRepoRoot);
@@ -61,6 +62,14 @@ sub prepare_sources {
 	die ("Must grab Boo implementation") if !-d "boo";
 	die ("Must grab Boo extensions") if !-d "boo-extensions";
 	die ("Must grab Unityscript implementation") if !-d "unityscript";
+}
+
+sub setup_env {
+
+	if ($ENV{UNITY_THISISABUILDMACHINE})
+	{
+		$SevenZip = "C:\7z\7z";
+	}
 }
 
 sub install_mono_libraries {
@@ -98,10 +107,6 @@ sub install_gkt_sharp {
 	{
 		print "== GTK Sharp $GTK_VERSION already installed\n";
 	}
-}
-
-sub setup_nant {
-	$nant = "\"$buildRepoRoot/dependencies/nant-0.93-nightly-2015-02-12/bin/NAnt.exe\"";
 }
 
 sub build_monodevelop 
