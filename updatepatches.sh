8a00e1e4a402e18ddb7f89846a4fa1b38e17678d
Lukasz <lukaszunity@users.noreply.github.com>
2016-01-19 12:18:16 +0100

    updatepatches.sh: Create mono-addin.patch.

--- updatepatches.sh
+++ updatepatches.sh
@@ -21,3 +21,12 @@ git checkout unity-trunk
 git pull origin unity-trunk
 
 git diff unity-trunk...unity-trunk-patch > ${CURRENT}/patches/debugger-libs.patch
+
+cd ${ROOT}/mono-addins
+
+git checkout unity-trunk-patch
+git pull origin unity-trunk-patch
+git checkout unity-trunk
+git pull origin unity-trunk
+
+git diff unity-trunk...unity-trunk-patch > ${CURRENT}/patches/mono-addins.patch
