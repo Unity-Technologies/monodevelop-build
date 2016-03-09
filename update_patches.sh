fa116ddfdf55dba8af5db625c01ab69f92788b31
Lukasz <lukaszunity@users.noreply.github.com>
2016-03-09 11:14:29 +0100

    Update branches in update_patches.sh script.

--- update_patches.sh
+++ update_patches.sh
@@ -4,8 +4,8 @@ CURRENT=`pwd`
 cd ..
 ROOT=`pwd`
 
-BASE_BRANCH="unity-trunk-monodevelop-5.10.0"
-PATCH_BRANCH="unity-trunk-patch-monodevelop-5.10.0"
+BASE_BRANCH="unity-trunk-monodevelop-5.x-future"
+PATCH_BRANCH="unity-trunk-patch-monodevelop-5.x-future"
 
 echo "Update patches with base branch ${BASE_BRANCH} and patch branch ${PATCH_BRANCH}"
 
